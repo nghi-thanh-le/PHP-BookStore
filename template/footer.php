8a07b102dc50755b8a0faa2964e0c9fd2f1f5d07
Nghi Le Thah <nghi.lethanh@outlook.com>
2016-01-19 17:36:13 +0200

    Upload to github

---
+++ template/footer.php
@@ -0,0 +1,20 @@
+      	<hr>
+
+      	<footer>
+        	<div class="text-muted pull-left">
+            	<a href="http://www.getboostrap.com" target="_blank">Bootstrap &amp; PHP &reg; Sample App</a>
+        	</div>
+        	<div class="text-muted pull-right">
+          		<a href="admin.php">Nghi Le Thanh</a> 2015
+        	</div>
+      	</footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="./bootstrap/js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
